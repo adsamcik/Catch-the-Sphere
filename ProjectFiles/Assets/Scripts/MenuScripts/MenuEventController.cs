a8a9f02424a0b828533ab5b6a1231033575d7776
Adam Novák <adsamcik@gmail.com>
2015-06-02 13:22:26 +0200

    Bugfix update

--- ProjectFiles/Assets/Scripts/MenuScripts/MenuEventController.cs
+++ ProjectFiles/Assets/Scripts/MenuScripts/MenuEventController.cs
@@ -2,6 +2,11 @@
 using System.Collections;
 
 public class MenuEventController : MonoBehaviour {
+	public UnityEngine.UI.Text HighscoreText;
+
+	void Start() {
+		HighscoreText.text = "highscore " + PlayerPrefs.GetInt("hs_normal");
+	}
 
 	public void LoadLevel(string name) {
 		Application.LoadLevel(name);
