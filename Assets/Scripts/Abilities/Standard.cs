25b3301b75ae513b77f9d8e9556de4ec453cdba8
adsamcik <adsamcik@users.noreply.github.com>
2016-06-24 17:19:50 +0200

    Abilities progress

--- Assets/Scripts/Abilities/Standard.cs
+++ Assets/Scripts/Abilities/Standard.cs
@@ -1,20 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Standard : MonoBehaviour {
-    public float speed = 10;
-    public float sqrspeed;
-    public new Rigidbody rigidbody;
+public class Standard : Ability {
+    const int MAX_SPEED = 50;
+    const int MIN_SPEED = 10;
+    const float POP_ANIMATION_LENGTH = 0.5f;
 
-    void Start() {
-        rigidbody = GetComponent<Rigidbody>();
+    Rigidbody rigidbody;
+    Transform transform;
+    float sqrspeed;
+
+    void FixedUpdate() {
+        if (rigidbody.velocity.sqrMagnitude < sqrspeed)
+            rigidbody.AddForce(rigidbody.velocity.normalized, ForceMode.Impulse);
+    }
+
+    public void Initialize(GameObject g) {
+        rigidbody = g.GetComponent<Rigidbody>();
+        transform = g.transform;
+        float speed = Random.Range(MIN_SPEED, MAX_SPEED);
         sqrspeed = speed * speed;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-	    if(rigidbody.velocity.sqrMagnitude < sqrspeed) {
-            rigidbody.AddForce(rigidbody.velocity.normalized, ForceMode.Impulse);
+
+    public void OnFieldEnter(GameObject g) {
+
+    }
+
+    public void OnFieldExit(GameObject g) {
+
+    }
+
+    public int Pop() {
+        return Mathf.RoundToInt(sqrspeed);
+    }
+
+    public void FixedUpdate(Rigidbody rigidbody) {
+
+    }
+
+    public IEnumerator PopAnimation() {
+        float mod = transform.localScale.x / POP_ANIMATION_LENGTH;
+        while (transform.localScale.x > 0) {
+            float val = Time.deltaTime * mod;
+            transform.localScale -= new Vector3(val, val, val);
+            yield return new WaitForEndOfFrame();
         }
-	}
+    }
 }
