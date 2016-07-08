76bfd20e75aec37ee0dfa0aba528228b38bbe6ae
adsamcik <adsamcik@gmail.com>
2016-07-08 11:56:42 +0200

    Refactoring

--- Assets/Scripts/rotate.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class rotate : MonoBehaviour {
-
-	void Update () {
-        transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
-	}
-}
