a67d69b73e8da126455820e1621b01f0cc899203
adsamcik <adsamcik@users.noreply.github.com>
2016-07-31 13:53:21 +0200

    Updated Menu functions

--- Assets/Scripts/MenuEventController.cs
+++ Assets/Scripts/MenuEventController.cs
@@ -13,6 +13,14 @@ public class MenuEventController : MonoBehaviour {
         SceneManager.LoadScene(name);
 	}
 
+    public void Journal() {
+
+    }
+
+    public void Stats() {
+
+    }
+
     public void Exit() {
         Application.Quit();
     }
