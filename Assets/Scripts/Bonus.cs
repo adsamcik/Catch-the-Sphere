687a747c9ab755f606f6d8c85e7cf897b7997dec
adsamcik <adsamcik@gmail.com>
2016-08-01 10:31:36 +0200

    Add Bonus class

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Bonus {
+    float timeLeft;
+    int value;
+    Func<Rigidbody, int> function;
+
+    public Bonus(float timeLeft, int value) {
+        this.timeLeft = timeLeft;
+        this.value = value;
+    }
+
+    public Bonus(float timeLeft, Func<Rigidbody, int> function) {
+        this.timeLeft = timeLeft;
+        this.function = function;
+    }
+
+    public bool UpdateTime(float timeDiff) {
+        timeLeft -= timeDiff;
+        return timeLeft > 0;
+    }
+
+    public int CountBonus(Rigidbody r) {
+        return function != null ? function(r) : value;
+    }
+}
