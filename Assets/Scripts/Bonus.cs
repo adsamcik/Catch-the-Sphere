792e1e12f22693b3b39c97d0e63b34b0b63346c4
adsamcik <adsamcik@gmail.com>
2016-08-01 12:06:53 +0200

    Improved Bonus class

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -3,26 +3,45 @@ using System.Collections;
 using System;
 
 public class Bonus {
+    readonly float totalTime;
     float timeLeft;
-    int value;
-    Func<Rigidbody, int> function;
+    readonly bool useTime;
 
-    public Bonus(float timeLeft, int value) {
+    readonly bool shouldDecay;
+    readonly int value;
+    readonly Func<Rigidbody, int> function;
+
+    public Bonus(int value, bool shouldDecay = false, float timeLeft = -1) {
+        this.useTime = timeLeft > 0;
         this.timeLeft = timeLeft;
+        this.totalTime = timeLeft;
         this.value = value;
+
+        if (!useTime && shouldDecay)
+            throw new Exception("Bonus must have limited lifetime when decay is enabled");
+        this.shouldDecay = shouldDecay;
     }
 
-    public Bonus(float timeLeft, Func<Rigidbody, int> function) {
+    public Bonus(Func<Rigidbody, int> function, bool shouldDecay = false, float timeLeft = -1) {
+        this.useTime = timeLeft > 0;
         this.timeLeft = timeLeft;
+        this.totalTime = timeLeft;
         this.function = function;
+
+        if (!useTime && shouldDecay)
+            throw new Exception("Bonus must have limited lifetime when decay is enabled");
+        this.shouldDecay = shouldDecay;
     }
 
     public bool UpdateTime(float timeDiff) {
+        if (!useTime)
+            return true;
         timeLeft -= timeDiff;
         return timeLeft > 0;
     }
 
     public int CountBonus(Rigidbody r) {
-        return function != null ? function(r) : value;
+        int bonus = function != null ? function(r) : value;
+        return shouldDecay ? Mathf.RoundToInt(bonus * (1 - (timeLeft / totalTime))) : bonus;
     }
 }
